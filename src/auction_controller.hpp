0c39f518b75850d8f7b0f8a48f3282ff73c77566
Daniel T. Schøn <41172992+dscho15@users.noreply.github.com>
2025-12-26 17:21:34 +0100

    Add detector scoring modes to auction controller

--- src/auction_controller.hpp
+++ src/auction_controller.hpp
@@ -6,6 +6,7 @@
 struct PhaseBidGroup {
   int phaseIndex;
   std::vector<std::string> lanes;
+  std::vector<std::string> detectors;
 };
 
 struct AuctionWeights {
@@ -13,15 +14,23 @@ struct AuctionWeights {
   double waitWeight;
 };
 
+enum class ScoringMode {
+  QueueWait,
+  Occupancy,
+  Flow,
+};
+
 class AuctionController {
  public:
   AuctionController(std::string trafficLightId,
                     std::vector<PhaseBidGroup> phaseGroups,
                     AuctionWeights weights,
+                    ScoringMode scoringMode,
                     int phaseDurationSeconds);
 
   void setWeights(AuctionWeights weights);
   AuctionWeights weights() const;
+  ScoringMode scoringMode() const;
 
   int selectWinningPhase() const;
   void applyPhaseIfDue(int currentSimStepSeconds);
@@ -32,6 +41,7 @@ class AuctionController {
   std::string trafficLightId_;
   std::vector<PhaseBidGroup> phaseGroups_;
   AuctionWeights weights_;
+  ScoringMode scoringMode_;
   int phaseDurationSeconds_;
   int lastPhaseChangeStep_;
 };
